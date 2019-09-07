a27850af45385c61d6a46698284e0884ea9d7dd8
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-07 13:42:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,25 +1,23 @@
-# class Song
-#   attr_accessor :name, :artist
-#   @@all=[]
+class Song
+  attr_accessor :name, :artist
+  @@all=[]
   
-#   def initialize(name)
-#     @name=name
-#     @@all << self
-#   end
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
   
-#   def self.all
-#     @@all
-#   end
+  def self.all
+    @@all
+  end
   
-#   def artist_name
-#     artist.name if artist
-#   end
-  
-# end
-
-class Song 
+  def artist_name
+    artist.name if artist
+  end
   
 end
+
+
 
 
 
