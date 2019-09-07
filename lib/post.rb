22faf1734132875df2bc75a4a2e249dd34a56bf6
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-07 13:54:25 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,10 +2,10 @@ class Post
 #   attr_accessor :title, :author
 #   @@all=[]
   
-#   def initialize(title)
-#     @title=title
-#     @@all << self
-#   end
+  def initialize(title)
+    @title=title
+    @@all << self
+  end
   
   def self.all
     @@all
